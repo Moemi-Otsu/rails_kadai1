f47a02fa39fe4c499e1dcefcad8f62646b6927f6
Moemi Ohtsu <s0623192@gmail.com>
2019-05-27 02:31:29 +0000

    configのapplication.rbを編集

--- config/application.rb
+++ config/application.rb
@@ -2,17 +2,18 @@ require_relative 'boot'
 
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Kadai
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    
+    #タイムゾーンの設定
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    
+    #本番環境のログレベルを、重要なものだけをだすレベルに変更
+    config.log_level = :info
   end
 end
