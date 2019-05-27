fe91495159d7b74523e65c53d725cdcbd9ae2a56
Moemi Ohtsu <s0623192@gmail.com>
2019-05-27 04:00:17 +0000

    new機能を作成

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,10 +1,20 @@
 class BlogsController < ApplicationController
   
   def index
+    @blogs = Blog.all
   end
   
   def new
     @blog = Blog.new
+  end
+  
+  def create
+    Blog.create(blog_params)
+    redirect_to new_blog_path
+  end
+  
+  def blog_params
+    params.require(:blog).permit(:content)
   end
   
 end
