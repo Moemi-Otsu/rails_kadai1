f38e7898be01d64fa33a50bf9a2693b1ba892c23
Moemi Ohtsu <s0623192@gmail.com>
2019-05-27 04:59:45 +0000

    show機能を作成

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -13,6 +13,12 @@ class BlogsController < ApplicationController
     redirect_to new_blog_path
   end
   
+  def show
+    @blog = Blog.find(params[:id])
+  end
+  
+  private
+  
   def blog_params
     params.require(:blog).permit(:content)
   end
